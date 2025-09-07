ee5f15a009b9e98c3c182a5256676bc9183bde85
naveenakasaragadda1 <naveenakasaragadda1@gmail.com>
2025-09-07 21:39:28 +0530

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://timesheet-backend-production-283f.up.railway.app/api',
+  baseURL: 'https://timesheet-server-gkd8.onrender.com/api',
 });
 
 // Attach token to every request
